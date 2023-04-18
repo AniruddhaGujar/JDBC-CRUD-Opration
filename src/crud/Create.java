3e0c92f12efdae0f85b65308b6790a966ee8c0d0
AniruddhaGujar <gujaraniruddha02@gmail.com>
2023-04-18 08:02:43 -0700

    JDBC CRUD Oprations

---
+++ src/crud/Create.java
@@ -0,0 +1,44 @@
+package crud;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Create {
+	
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost:3306/javabatch";
+		String username = "root";
+		String password = "root";
+		
+		String sql = "INSERT INTO car VALUE (4, 'Mahindra')";
+		Connection connection = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(url, username, password);
+			Statement statement = connection.createStatement();
+			statement.execute(sql);
+			System.out.println("Values Imported");
+			
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+
+}
